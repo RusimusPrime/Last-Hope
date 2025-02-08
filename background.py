f4bacc4fb37e766472a1235a36e9c3875cb550ce
BillDecypher <ushakov.f.12345@gmail.com>
2025-02-08 19:04:46 +0300

    backgrounf_update

--- background.py
+++ background.py
@@ -7,9 +7,13 @@ class Background(pygame.sprite.Sprite):
         self.image = pygame.image.load('sprites/background_1_1.png')
         self.image = pygame.transform.scale(self.image,
                                                  (1200, 800))
+        self.back = pygame.image.load('sprites/back.png')
+        self.back = pygame.transform.scale(self.back,
+                                            (1200, 800))
         self.image_index = 1
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
+        self.off = False
         self.surface = pygame.Surface((self.rect.width, self.rect.height), pygame.SRCALPHA)
         self.screen_rect = self.screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
@@ -17,6 +21,7 @@ class Background(pygame.sprite.Sprite):
         self.rect.bottom = self.screen_rect.bottom
 
     def output(self, mask):
+        self.screen.blit(self.back, self.rect)
         self.screen.blit(self.image, self.rect)
         if pygame.sprite.collide_mask(self, mask):
             if self.image_index != 4:
@@ -36,3 +41,4 @@ class Background(pygame.sprite.Sprite):
 
         if self.image_index == 0:
             self.image_index = 1
+
