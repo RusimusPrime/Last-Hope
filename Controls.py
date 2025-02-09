57c03672e8ed7f220f0d2042c59dbd5a7a09652b
BillDecypher <ushakov.f.12345@gmail.com>
2025-02-09 19:42:17 +0300

    updated

--- Controls.py
+++ Controls.py
@@ -39,9 +39,10 @@ def events(cheracter, back):
                 cheracter.rect.centery = cheracter.rect.centery + 9
                 cheracter.rect.centerx = cheracter.rect.centerx + 36
 
-def update(screen, bg_color, lamp, back, enemy):
+def update(screen, bg_color, lamp, back, enemy, enemy2):
     screen.fill(bg_color)
     back.output(lamp)
     screen.blit(enemy.current_image, enemy.rect)
+    screen.blit(enemy2.current_image, enemy2.rect)
     lamp.output(back)
     pygame.display.flip()
--- game_test.py
+++ game_test.py
@@ -4,36 +4,74 @@ import Controls
 from cheracter import Lamp
 from background import Background
 from Villian import MinorVillain
-import Hero
-import Map
-def work():
+import cv2
 
+def screamer(window):
+    video = cv2.VideoCapture("images/screem.mp4")
+    success, video_image = video.read()
+    fps = video.get(cv2.CAP_PROP_FPS)
+
+    clock = pygame.time.Clock()
+
+    run = success
+    while run:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        success, video_image = video.read()
+        if success:
+            video_surf = pygame.image.frombuffer(
+                video_image.tobytes(),
+                video_image.shape[1::-1],
+                "BGR"
+            )
+        else:
+            run = False
+        window.blit(video_surf, (0, 0))
+        pygame.display.flip()
+
+
+def work():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption('Last Hope')
+    pygame.display.set_caption("Last Hope")
     bg_color = (255, 255, 255)
     clock = pygame.time.Clock()
     fps = 20
     lamp = Lamp(screen)
     back = Background(screen)
-    enemy = MinorVillain(300, 500)
+    enemy = MinorVillain(300, 510)
+    enemy2 = MinorVillain(300, 160)
     all_sprites = pygame.sprite.Group()
     all_sprites.add(back)
     all_sprites.add(lamp)
-    #all_sprites.add(enemy)
-    #print(all_sprites)
 
     while True:
+        if (not lamp.hide_index) and enemy.are_sprites_touching(lamp) or enemy2.are_sprites_touching(lamp):
+            screamer(screen)
+            return True
         Controls.events(lamp, back)
         lamp.update(back)
         screen.fill(bg_color)
         all_sprites.draw(screen)
-        Controls.update(screen, bg_color, lamp, back, enemy)
+        Controls.update(screen, bg_color, lamp, back, enemy, enemy2)
         enemy.update(lamp)
-        if enemy.are_sprites_touching(lamp):
-            print('colide')
-        else:
-            print('NO')
+        enemy2.update(lamp)
+
         clock.tick(fps)
 
-work()
+start = 1
+while True:
+    if start == 1:
+        work()
+        start = 0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                start = 1
+
+
