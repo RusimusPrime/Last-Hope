f40674c8b7a8b3d921704f5ec50624cbdaa8fda4
RusimusPrime <ruslankosov9@gmail.com>
2025-02-09 19:11:19 +0300

    update

---
+++ Controls.py
@@ -0,0 +1,47 @@
+import pygame, sys
+
+def events(cheracter, back):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            cheracter.stand_check = False
+            #hide
+            if event.key == pygame.K_e:
+                cheracter.hide_index = True
+                cheracter.rect.centery = cheracter.rect.centery - 9
+                cheracter.rect.centerx = cheracter.rect.centerx - 36
+            # right
+            if event.key == pygame.K_d:
+                cheracter.move_right = True
+                #cheracter.move_left = False
+            # left
+            if event.key == pygame.K_a:
+                #cheracter.move_right = False
+                cheracter.move_left = True
+            #jump
+            if event.key == pygame.K_SPACE and pygame.sprite.collide_mask(cheracter, back):
+                cheracter.move_jump = True
+
+        elif event.type == pygame.KEYUP:
+            cheracter.stand_check = True
+            # right
+            if event.key == pygame.K_d:
+                cheracter.move_right = False
+
+            # left
+            if event.key == pygame.K_a:
+                cheracter.move_left = False
+            # hide
+            if event.key == pygame.K_e:
+                cheracter.hide_index = False
+                cheracter.hide_count = 0
+                cheracter.rect.centery = cheracter.rect.centery + 9
+                cheracter.rect.centerx = cheracter.rect.centerx + 36
+
+def update(screen, bg_color, lamp, back, enemy):
+    screen.fill(bg_color)
+    back.output(lamp)
+    screen.blit(enemy.current_image, enemy.rect)
+    lamp.output(back)
+    pygame.display.flip()
---
+++ background.py
@@ -0,0 +1,46 @@
+import pygame
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, screen):
+        super().__init__()
+        self.screen = screen
+        self.image = pygame.image.load('sprites/background_1_1.png')
+        self.image = pygame.transform.scale(self.image,
+                                                 (1200, 800))
+        self.back = pygame.image.load('sprites/back.png')
+        self.back = pygame.transform.scale(self.back,
+                                            (1200, 800))
+        self.image_index = 1
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.off = False
+        self.surface = pygame.Surface((self.rect.width, self.rect.height), pygame.SRCALPHA)
+        self.screen_rect = self.screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.center = float(self.rect.centerx)
+        self.rect.bottom = self.screen_rect.bottom
+
+    def output(self, mask):
+        # отрисовка фонов
+        self.screen.blit(self.back, self.rect)
+        self.screen.blit(self.image, self.rect)
+        # анимация открытия дверей
+        if pygame.sprite.collide_mask(self, mask):
+            if self.image_index != 4:
+                self.image = pygame.image.load(f'sprites/background_1_{self.image_index}.png')
+                self.image = pygame.transform.scale(self.image,
+                                                    (1200, 800))
+
+                self.image_index += 1
+            if self.image_index == 4:
+                self.image_index = 3
+        else:
+            if self.image_index != 0:
+                self.image = pygame.image.load(f'sprites/background_1_{self.image_index}.png')
+                self.image = pygame.transform.scale(self.image,
+                                                    (1200, 800))
+                self.image_index -= 1
+
+        if self.image_index == 0:
+            self.image_index = 1
+
