fa5070c2980ef69e9f2d4796891a9e04b7090ea0
RusimusPrime <ruslankosov9@gmail.com>
2025-02-09 19:26:01 +0300

    update

--- game_test.py
+++ game_test.py
@@ -4,13 +4,39 @@ import Controls
 from cheracter import Lamp
 from background import Background
 from Villian import MinorVillain
-import Hero
-import Map
-def work():
+import cv2
 
+def screamer(window):
+    video = cv2.VideoCapture("images/screem.mp4")  # Установите свое  видео
+    success, video_image = video.read()
+    fps = video.get(cv2.CAP_PROP_FPS)
+
+    clock = pygame.time.Clock()
+
+    run = success
+    while run:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        success, video_image = video.read()
+        if success:
+            video_surf = pygame.image.frombuffer(
+                video_image.tobytes(),
+                video_image.shape[1::-1],
+                "BGR"
+            )
+        else:
+            run = False
+        window.blit(video_surf, (0, 0))
+        pygame.display.flip()
+
+
+def work():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption('Last Hope')
+    pygame.display.set_caption("Last Hope")
     bg_color = (255, 255, 255)
     clock = pygame.time.Clock()
     fps = 20
@@ -20,21 +46,19 @@ def work():
     all_sprites = pygame.sprite.Group()
     all_sprites.add(back)
     all_sprites.add(lamp)
-    #all_sprites.add(enemy)
-    #print(all_sprites)
 
     while True:
+        if enemy.are_sprites_touching(lamp):
+            screamer(screen)
+            pygame.quit()
         Controls.events(lamp, back)
         lamp.update(back)
         screen.fill(bg_color)
         all_sprites.draw(screen)
         Controls.update(screen, bg_color, lamp, back, enemy)
         enemy.update(lamp)
-        print(enemy.rect)
-        if enemy.are_sprites_touching(lamp):
-            print('colide')
-        else:
-            print('NO')
+
         clock.tick(fps)
+
 
 work()
