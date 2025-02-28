dbbddf03816e362f0c9e89aad46ade94dc0a3551
RusimusPrime <ruslankosov9@gmail.com>
2025-02-28 21:08:55 +0300

    update

--- datа/cheracter.py
+++ datа/cheracter.py
@@ -107,7 +107,7 @@ class Lamp(pygame.sprite.Sprite):
             self.aura_rect.centerx = self.rect.centerx + 35
             self.aura_rect.centery = self.rect.centery + 5
         else:
-            self.aura = pygame.transform.scale(pygame.image.load("data/images/aura.png"),
+            self.aura = pygame.transform.scale(pygame.image.load("datа/images/aura.png"),
                                                (2048 * 3, 2048 * 3))
             self.aura_rect = self.aura.get_rect()
             self.aura_rect.centerx = self.rect.centerx
