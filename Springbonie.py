8cf5d81a7896319448ec5b698a18564c0a184771
BillDecypher <ushakov.f.12345@gmail.com>
2025-03-01 16:03:16 +0300

    Bonie_added

---
+++ Springbonie.py
@@ -0,0 +1,50 @@
+import pygame
+
+
+class Bonnie(pygame.sprite.Sprite):
+    def __init__(self, x, y, on_the_screen=True, *group):
+        super().__init__(*group)
+        self.on_the_screen = on_the_screen
+        self.rect = pygame.transform.scale(pygame.image.load("SpringBonnie_sprites/1.png"), (128, 128)).get_rect()
+        self.direction = "right"
+        self.count = 0
+        self.move_count = 0
+        self.rect.x, self.rect.y = x, y
+        self.size = (114, 200)
+        self.current_image = pygame.transform.scale(pygame.image.load("SpringBonnie_sprites/1.png"), self.size)
+        self.sprites_right = []
+        self.sprites_left = []
+        for i in range(1, 21):
+            self.sprites_left += [pygame.transform.scale(pygame.image.load(f"SpringBonnie_sprites/{i}.png"), self.size)]
+
+        for i in range(1, 21):
+            self.sprites_right += [
+                pygame.transform.flip(pygame.transform.scale(pygame.image.load(f"SpringBonnie_sprites/{i}.png"), self.size), True, False)]
+
+    # 300 - 900
+    def update(self, hero):
+        if (not hero.hide_index) and -200 < hero.rect.x - self.rect.x < 0 and self.direction == "left":
+            self.rect.x += - 7
+            self.count = (self.count + 1) % 60
+            self.current_image = self.sprites_left[self.count // 3]
+
+        elif (not hero.hide_index) and 0 < hero.rect.x - self.rect.x < 200 and self.direction == "right":
+            self.rect.x += 7
+            self.count = (self.count + 1) % 60
+            self.current_image = self.sprites_right[self.count // 3]
+
+        else:
+            if self.direction == "right":
+                self.rect.x += 3
+                self.move_count = (self.move_count + 1) % 60
+                self.current_image = self.sprites_right[self.move_count // 3]
+
+            elif self.direction == "left":
+                self.rect.x += -3
+                self.move_count = (self.move_count + 1) % 60
+                self.current_image = self.sprites_left[self.move_count // 3]
+            if self.rect.x < 100 or self.rect.x > 1000:
+                self.direction = "right" if self.direction == "left" else "left"
+
+    def are_sprites_touching(self, sprite2):
+        return self.rect.colliderect(sprite2.rect)
--- game.py
+++ game.py
@@ -1,134 +1,93 @@
 import pygame
-import sys
+import sqlite3
+import Controls
+from cheracter import Lamp
+from background import Background
+from Villian import MinorVillain
+from Springbonie import Bonnie
+import cv2
 import time
+import pyttsx3
+import random
 
-pygame.init()
 
-time = pygame.time.Clock()
-i = 0
-b = 0
-jump_r = [
-    pygame.image.load('sprites/jump1.png'),
-    pygame.image.load('sprites/jump2.png'),
-    pygame.image.load('sprites/jump3.png'),
-    pygame.image.load('sprites/jump4.png'),
-]
-jump_l = [
+def screamer(window):
+    video = cv2.VideoCapture("datа/screem.mp4")
+    success, video_image = video.read()
+    fps = video.get(cv2.CAP_PROP_FPS)
+    clock = pygame.time.Clock()
+    run = success
+    while run:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-    pygame.image.load('sprites/jump5.png'),
-    pygame.image.load('sprites/jump6.png'),
-    pygame.image.load('sprites/jump7.png'),
-    pygame.image.load('sprites/jump8.png'),
-]
-beg_r = [
-    pygame.image.load('sprites/beg1.png'),
-    pygame.image.load('sprites/beg2.png'),
-    pygame.image.load('sprites/beg3.png'),
-]
-beg_l = [
-    pygame.image.load('sprites/beg4.png'),
-    pygame.image.load('sprites/beg5.png'),
-    pygame.image.load('sprites/beg6.png'),
-]
-stoyka = [
-    pygame.image.load('sprites/stoyka1.png'),
-    pygame.image.load('sprites/stoyka2.png'),
-]
-x = 200
-
-player_x = 10
-player_y = 450
-
-player_anim_count_beg = 0
-player_anim_count_jump = 0
-player_anim_count_jump_2 = 0
-player_anim_count_stoyka = 0
-player_anim_count_stoyka_2 = 0
-
-screen = pygame.display.set_mode((900, 900))
-pygame.display.set_caption('My game')
-isJump = False
-jumpcount = 10
-player_speed = 10
-
-while True:
-    keys = pygame.key.get_pressed()
-    screen.fill('white')
-    if keys[pygame.K_a] and player_x > 10 and jumpcount == 10:
-        screen.blit(beg_l[player_anim_count_beg], (player_x, player_y))
-        player_x -= player_speed
-    elif keys[pygame.K_d] and player_x < 870 and jumpcount == 10:
-        screen.blit(beg_r[player_anim_count_beg], (player_x, player_y))
-        player_x += player_speed
-    elif jumpcount == 10:
-        screen.blit(stoyka[player_anim_count_stoyka], (player_x, player_y))
-
-    if player_anim_count_beg == 2:
-        player_anim_count_beg = 0
-    else:
-        player_anim_count_beg += 1
-
-    if isJump == False:
-        # if keys[pygame.K_a] and player_x > 10 :
-        # screen.blit(beg_l[player_anim_count_beg],(player_x,player_y))
-        # player_x -= player_speed
-        # elif keys[pygame.K_d] and player_x <870:
-        # screen.blit(beg_r[player_anim_count_beg],(player_x,player_y))
-        # player_x += player_speed
-        if keys[pygame.K_SPACE]:
-            isJump = True
-        # else :
-        # screen.blit(stoyka[player_anim_count_stoyka],(player_x,player_y))
-
-    if jumpcount >= -10 and isJump == True:
-        if jumpcount < 0:
-            player_y += (jumpcount ** 2) / 6
+        success, video_image = video.read()
+        if success:
+            video_surf = pygame.image.frombuffer(
+                video_image.tobytes(),
+                video_image.shape[1::-1],
+                "BGR"
+            )
         else:
-            player_y -= (jumpcount ** 2) / 6
-        if keys[pygame.K_d] and player_x < 870:
-            player_x += player_speed
-            screen.blit(jump_r[player_anim_count_jump], (player_x, player_y))
-        elif keys[pygame.K_a] and player_x > 10 and keys[pygame.K_SPACE]:
-            player_x -= player_speed
-            screen.blit(jump_l[player_anim_count_jump], (player_x, player_y))
-        else:
-            screen.blit(jump_r[player_anim_count_jump], (player_x, player_y))
-        player_anim_count_jump_2 += 0.15
-        jumpcount -= 1
+            run = False
+        window.blit(video_surf, (0, 0))
+        pygame.display.flip()
 
-    else:
-        isJump = False
-        jumpcount = 10
-        player_anim_count_jump_2 = 0
 
-    if player_anim_count_jump_2 // 1 == 0:
-        player_anim_count_jump = 0
-    elif player_anim_count_jump_2 // 1 == 1:
-        player_anim_count_jump = 1
-    elif player_anim_count_jump_2 // 1 == 2:
-        player_anim_count_jump = 2
-    elif player_anim_count_jump_2 // 1 == 3:
-        player_anim_count_jump = 3
-    elif player_anim_count_jump_2 // 1 == 4:
-        player_anim_count_jump = 0
-        player_anim_count_jump_2 = 0
-
-    # if player_anim_count_stoyka_2  == 2:
-    # player_anim_count_stoyka =
-    if player_anim_count_stoyka_2 // 1 == 0:
-        player_anim_count_stoyka = 0
-        player_anim_count_stoyka_2 += 0.2
-    elif player_anim_count_stoyka_2 // 1 == 1:
-        player_anim_count_stoyka = 1
-        player_anim_count_stoyka_2 += 0.2
-    elif player_anim_count_stoyka_2 // 1 == 2:
-        player_anim_count_stoyka = 0
-        player_anim_count_stoyka_2 = 0
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    pygame.display.update()
-    time.tick(12)
+def work(n):
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Last Hope")
+    bg_color = (255, 255, 255)
+    clock = pygame.time.Clock()
+    fps = 30
+    lamp = Lamp(screen)
+    back = Background(screen, n)
+    koef = random.randint(1, 5)
+    enemy = Bonnie(300, 510) if koef == 5 else MinorVillain(300, 510)
+    koef = random.randint(1, 5)
+    enemy2 = MinorVillain(300, 160) if koef == 5 else MinorVillain(300, 160)
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(back)
+    all_sprites.add(lamp)
+    e_count = 0
+    start_time = time.time()
+    while True:
+        if (not lamp.hide_index) and (enemy.are_sprites_touching(lamp) or enemy2.are_sprites_touching(lamp)):
+            screamer(screen)
+            engine = pyttsx3.init()
+            best_time = str(abs(start_time - time.time()))[:str(start_time - time.time()).find('.') - 1]
+            text = f"Вы прожили {best_time} секунд"
+            engine.say(text)
+            engine.runAndWait()
+            con = sqlite3.connect("datа/time.sqlite")
+            con.execute("""INSERT INTO best_time (time) VALUES(?)""", (best_time,))
+            con.commit()
+            return True
+        if lamp.hide_index:
+            e_count += 1
+            lamp.update_aura(e_count)
+            if e_count > 100:
+                screamer(screen)
+                engine = pyttsx3.init()
+                best_time = str(abs(start_time - time.time()))[:str(start_time - time.time()).find('.') - 1]
+                text = f"Вы прожили {best_time} секунд"
+                engine.say(text)
+                engine.runAndWait()
+                con = sqlite3.connect("datа/time.sqlite")
+                con.execute("""INSERT INTO best_time (time) VALUES(?)""", (best_time,))
+                con.commit()
+                return True
+        elif e_count != 0:
+            e_count = 0
+            lamp.update_aura(e_count)
+        Controls.events(lamp, back)
+        enemy.update(lamp)
+        enemy2.update(lamp)
+        lamp.update(back)
+        screen.fill(bg_color)
+        all_sprites.draw(screen)
+        Controls.update(screen, bg_color, lamp, back, enemy, enemy2)
+        clock.tick(fps)
